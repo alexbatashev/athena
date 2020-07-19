a020c8586af86ec47c6b7e63299ea6f609200caa
Alexander Batashev <alexbatashev@outlook.com>
2020-07-19 12:05:40 +0300

    build: remove unused dependencies (#144)

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,22 +1,5 @@
 # This file contains mandatory dependencies
 
-# yaml-cpp is used to parse integration test config files
-set(YAML_CPP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(YAML_CPP_BUILD_CONTRIB OFF CACHE BOOL "" FORCE)
-set(YAML_CPP_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
-AthenaAddDependency(
-        TARGET_NAME yaml-cpp
-        GIT_URL https://github.com/jbeder/yaml-cpp.git
-        GIT_TAG yaml-cpp-0.6.2
-)
-
-# Boost filesystem is used to process paths in integration tests
-AthenaAddDependency(
-        TARGET_NAME Boost
-        PACKAGE Boost
-        COMPONENTS filesystem
-)
-
 # Google test is the primary testing framework for the project
 AthenaAddDependency(
         TARGET_NAME googletest
